87107bc987c7d6aa29abb7776e8e968a1ec275a8
eivanova <yeivanova@gmail.com>
2023-01-18 12:05:25 +0100

    add statistics first screen

--- src/pages/statistics/statistics.tsx
+++ src/pages/statistics/statistics.tsx
@@ -1,9 +1,51 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import styles from "./statistics.module.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "../../services/store";
+import { Link } from "react-router-dom";
+import * as d3 from "d3";
 
 export const StatisticsPage: FC = () => {
-	return (
-		<div>
-			<h1>Статистика</h1>
-		</div>
-	);
+  const quantity = useSelector(
+    (state: RootState) => state.coasters.items
+  ).length;
+
+  useEffect(() => {
+    d3.select("#text")
+      .transition()
+      .tween("text", () => {
+        const interpolator = d3.interpolateNumber(quantity / 2, quantity);
+        return function (t: number) {
+          d3.select(this).text(Math.round(interpolator(t)));
+        };
+      })
+      .duration(1000);
+  }, [quantity]);
+
+  return (
+    <main className={styles.main}>
+      <Link to="/" className={styles.back_home}>
+        На главную
+      </Link>
+      <h1>
+        На данный момент эта коллекция насчитывает
+        <div id="counter" className={styles.quantity}>
+          <svg width="100%" height="320" viewBox="0 0 530 320">
+            <text
+              id="text"
+              className={styles.text}
+              x="50%"
+              y="50%"
+              dominant-baseline="middle"
+              text-anchor="middle"
+            ></text>
+          </svg>
+        </div>
+        <span className={styles.subtext}>бирдекелей</span>
+      </h1>
+      <p className={styles.h1}>
+        и вся статистика на этой странице рассчитана из этого числа
+      </p>
+    </main>
+  );
 };
