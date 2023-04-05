3c366d532e4eab98fb691959719f6f5ad6caaa47
eivanova <yeivanova@gmail.com>
2023-04-05 11:59:50 +0200

    add countries list

--- src/pages/statistics/countries.tsx
+++ src/pages/statistics/countries.tsx
@@ -11,34 +11,31 @@ type TSectionCountriesProps = {
   setActiveSection: (value: string) => void;
 };
 
-export const SectionCountries: FC<TSectionCountriesProps> = ({ setActiveSection }) => {
+export const SectionCountries: FC<TSectionCountriesProps> = ({
+  setActiveSection,
+}) => {
   const { isDesktop } = useContext(DeviceContext);
   const [ref, inView] = useInView({ threshold: 0.1 });
   const items = useSelector((state: RootState) => state.coasters.items);
   const params = useSelector((state: RootState) => state.coasters.params);
   const quantity = items.length;
+  const [countryData, setCountryData] = useState<TChartData[]>([]);
 
-  const uniqueTypeParams = (): TChartData[] => {
+  const uniqueCountryParams = (): TChartData[] => {
     const dataset = [] as TChartData[];
-    const othersQuantity = items.filter(
-      (item) => item.shape !== "Квадрат" && item.shape !== "Круг"
-    ).length;
 
-    params.shape.forEach((param) => {
-      if (param !== "Квадрат" && param !== "Круг") {
-        const value = Number(
-          (items.filter((item) => item.shape === param).length * 100) /
-            othersQuantity
-        ).toFixed(1);
+    params.country.forEach((param) => {
+      const value = Number(
+        (items.filter((item) => item.country === param).length * 100) / quantity
+      ).toFixed(1);
 
-        dataset.push({
-          label: param,
-          value: +value,
-        });
-      }
+      dataset.push({
+        label: param,
+        value: +value,
+      });
     });
 
-    dataset.sort((a, b) => (a < b) ? 1 : -1);
+    dataset.sort((a, b) => (a.label > b.label ? 1 : -1));
     return dataset;
   };
 
@@ -50,7 +47,7 @@ export const SectionCountries: FC<TSectionCountriesProps> = ({ setActiveSection 
 
   useEffect(() => {
     if (items.length > 0) {
-      
+      setCountryData(uniqueCountryParams());
     }
   }, [items, quantity]);
 
@@ -60,7 +57,17 @@ export const SectionCountries: FC<TSectionCountriesProps> = ({ setActiveSection 
       className={cn(styles.screen, styles.screen_4)}
       ref={ref}
     >
-      test
+      <div className={cn(styles.col, styles.countries_col)}>
+        <ul className={styles.countries_list}>
+          {countryData.map((country, index) => {
+            return (
+              <li className={styles.country_item} key={index}>
+                {country.label}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
   );
 };
